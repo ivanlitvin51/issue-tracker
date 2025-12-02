9d2fa300ee5e98b2b13fe210a7e6a59fa6230337
Ivan Litvin <ivanlitvin51@gmail.com>
2025-12-03 01:07:25 +0700

    extracted the Issue into a separate class

--- src/main.js
+++ src/main.js
@@ -1,60 +1,101 @@
 const addIssueButton = document.getElementById("add-issue-button");
-const issuesList = document.getElementById("issues-list")
+const issuesList = document.getElementById("issues-list");
+const issues = [];
 
-function deleteIssue(issue) {
-    issue.parentNode.removeChild(issue);
-}
-
-function refreshIssues(issues, cur_index) {
-    for (const elem of issues) {
-        cur_index++;
-    }
-    return cur_index;
-}
-
+// Issue
 class Issue {
+    constructor(text) {
+        this.text = text;
+        this.index = issues.length + 1;
 
+        // Create element
+        this.element = document.createElement("li");
+        this.element.className = "issue-item";
+
+        // Issue index
+        this.numberSpan = document.createElement("span");
+        this.numberSpan.className = "issue-span";
+        this.numberSpan.textContent = this.index + ". ";
+
+        // Issue text
+        this.textSpan = document.createElement("span");
+        this.textSpan.className = "issue-text";
+        this.textSpan.textContent = this.text;
+
+        // Delete button
+        this.deleteButton = document.createElement("button");
+        this.deleteButton.className = "btn btn-secondary";
+        this.deleteButton.id = "delete-issue-btn";
+        this.deleteButton.type = "button";
+        this.deleteButton.textContent = "Удалить";
+
+        // Element
+        this.element.appendChild(this.numberSpan);
+        this.element.appendChild(this.textSpan);
+        this.element.appendChild(this.deleteButton);
+
+        // Delete issue
+        this.deleteButton.addEventListener("click", () => {
+            this.delete();
+        });
+    }
+
+    // Set index
+    setIndex(newIndex) {
+        this.index = newIndex;
+        this.numberSpan.textContent = this.index + ". ";
+    }
+
+    // Delete
+    delete() {
+        this.element.remove();
+
+        const indexInIssuesList = issues.indexOf(this);
+        issues.splice(indexInIssuesList, 1);
+
+        refreshIndexes();
+    }
 }
 
+// Timeout show input
+function showNewIssueInput(input) {
+    setTimeout(() => input.classList.add("show"), 50);
+}
+
+// Renumber indexes
+function refreshIndexes() {
+    issues.forEach((issue, i) => {
+        issues.setIndex(i + 1);
+    });
+}
+
+// Create new issue
 function newIssue() {
     let newIssueInput = document.createElement("input");
     newIssueInput.type = "text";
     newIssueInput.className = "issue-input";
     issuesList.appendChild(newIssueInput);
-    setTimeout(() => newIssueInput.classList.add("show"), 10);
+    showNewIssueInput(newIssueInput);
 
     newIssueInput.focus();
 
     newIssueInput.addEventListener("keydown", (event) => {
-        const text = newIssueInput.value;
-        if (event.key == "Enter") {
-            if (text.trim() == "") {
+        const text = newIssueInput.value.trim();
+        if (event.key === "Enter") {
+            if (text.trim() === "") {
                 issuesList.removeChild(newIssueInput);
                 return;
             }
-            const newIssue = document.createElement("li");
-            newIssue.className = "issue-item";
-            newIssue.textContent = `${index++}. ${text}`;
+            const issue = new Issue(text);
+            issues.push(issue);
 
-            const issueDelete = document.createElement("button");
-            issueDelete.className = "btn btn-secondary";
-            issueDelete.id = "delete-issue-btn";
-            issueDelete.textContent = "Удалить";
-            newIssue.appendChild(issueDelete);
-            issueDelete.type = "button";
+            issuesList.replaceChild(issue.element, newIssueInput);
+            refreshIndexes();
 
-            issuesList.replaceChild(newIssue, newIssueInput);
-
-            issueDelete.addEventListener("click", () => {deleteIssue(newIssue), refreshIssues(issuesList, index)});
             return;
         }
     });
 }
 
-let index = 1;
 addIssueButton.addEventListener("click", newIssue);
 console.log("main loaded")
-console.log(issuesList.children.length);
-if (issuesList.children.length == 0) {
-
-}
